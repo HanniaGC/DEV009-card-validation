23206196c8f01cabe2a10c1c51fbf7528a373ad7
HanniaGC <hannia_gc@outlook.com>
2023-06-08 10:42:22 -0600

    ruebas isvalid y maskify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import validator from './validator.js';
 
-console.log(validator);
+function validateCard(event) {
+  event.preventDefault(); 
+
+  const numeroTarjeta = document.querySelector('input[name="numero_tarjeta"]').value;
+
+  const maskedNumber = validator.maskify(numeroTarjeta);
+  console.log(maskedNumber);
+
+  const isValid = validator.isValid(numeroTarjeta);
+  console.log(isValid);
+
+}
+
+function mostrarSeccionDonar() {
+  const donarSection = document.getElementById("secciondonar");
+  donarSection.style.display = "block";
+
+  const contenidoSection = document.getElementById("contenido");
+  contenidoSection.style.display = "none";
+}
+
+const contenidoSection = document.getElementById("contenido");
+contenidoSection.addEventListener("click", mostrarSeccionDonar);
+
+const form = document.querySelector('form');
+form.addEventListener('submit', validateCard);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,33 @@
 const validator = {
-  // ...
+  maskify: function (creditCardNumber) {
+    const lastFourDigits = creditCardNumber.slice(-4);
+    const maskedDigits = "*".repeat(creditCardNumber.length - 4);
+    return maskedDigits + lastFourDigits;
+  },
+
+  isValid: function (creditCardNumber) {
+    const trimmedNumber = creditCardNumber.replace(/\s/g, "");
+
+    let sum = 0;
+    let isSecondDigit = false;
+
+    for (let i = trimmedNumber.length - 1; i >= 0; i--) {
+      let digit = parseInt(trimmedNumber.charAt(i), 10);
+
+      if (isSecondDigit) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+
+      sum += digit;
+      isSecondDigit = !isSecondDigit;
+    }
+
+    return sum % 10 === 0;
+  },
+
 };
 
 export default validator;
